f62381a70ed73f29f81f46525a17c8f72371cf63
Chie Stroud <urirunrunkoko@gmail.com>
2021-01-23 11:08:39 -0600

    working with cakkbacks

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('Hello!!!!!!!');
+// console.log('Hello!!!!!!!');
+function firstOne(user) {
+  return `Hello ${user}`;
+}
+
+function secondOne(user) {
+  return `Howdy ${user}`;
+}
+
+function thirdOne(user, cb) {
+  return cb(user);
+}
+
+thirdOne('Chie', firstOne);
+thirdOne('Chie', secondOne);
