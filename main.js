521feb922fefb55933195f02a209fdfa90b82317
Chie Stroud <urirunrunkoko@gmail.com>
2021-01-23 10:57:13 -0600

    tested selectors

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log('Hello!!!!!!!');
+// console.log('Hello!!!!!!!');
+const mainButton = document.getElementById('main-btn');
+console.log(mainButton);
+
+const mainButton2 = document.querySelector('#main-btn');
+console.log(mainButton2);
